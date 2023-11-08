9278926ac1d5e09cad1a4967aef838d4a8f5d6d4
pythonbyfebash <pythonbyfebash@gmail.com>
2023-11-08 07:59:12 +0530

    complete todo functionality

--- todoweb.py
+++ todoweb.py
@@ -4,14 +4,20 @@ import todo_functions as tf
 todos_list = tf.get_todos()
 def add_todo():
     todo = st.session_state['todo_ip'] + '\n'
-    todos_list.append(todo)
-    tf.write_todos(todos_list)
+    if todo not in todos_list:
+        todos_list.append(todo)
+        tf.write_todos(todos_list)
 
 st.title('My Todo App')
 
 
-for todo in todos_list:
-    st.checkbox(todo)
+for index,todo in enumerate(todos_list):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos_list.pop(index)
+        tf.write_todos(todos_list)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input('',placeholder='Enter a todo...',key='todo_ip',
               on_change=add_todo)
